29a1427acafbdde694f036302c6328365f97156a
Godwin <godwin668@gmail.com>
2013-02-23 14:47:28 +0800

    *) Move stat codes to saperate file.

---
+++ src/main/webapp/static/idocv/js/stat.js
@@ -0,0 +1,23 @@
+/* ---------------------------------------------------------------------- */
+/*	Google Analytics
+/* ---------------------------------------------------------------------- */
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38005269-2']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+/* ---------------------------------------------------------------------- */
+/*	StatCounter
+/* ---------------------------------------------------------------------- */
+var sc_project=8658950; 
+var sc_invisible=1; 
+var sc_security="d4122c0b"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
