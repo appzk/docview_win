6562e73535cf8892cd50cdbf01669dbb7701dbd7
Godwin <godwin668@gmail.com>
2013-03-23 12:54:08 +0800

    *) Add app index

--- src/main/java/com/idocv/docview/controller/MainController.java
+++ src/main/java/com/idocv/docview/controller/MainController.java
@@ -1,15 +1,19 @@
 package com.idocv.docview.controller;
 
-import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("")
+//@Controller
+//@RequestMapping("")
 public class MainController {
 
 	@RequestMapping("")
-	public String HOME() {
-		return "redirect:/user/doclist.html";
+	public String HOME(HttpServletRequest req) {
+		StringBuffer sb = req.getRequestURL();
+		String app = sb.toString().replaceFirst("(http://)?(\\w+)\\.idocv.com/?.*", "$2");
+		System.out.println(sb);
+		return "redirect:/app/index.html";
 	}
 
 }
