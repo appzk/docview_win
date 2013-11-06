b151869a91e3166b558d112963066dab1f29118b
Godwin <godwin668@gmail.com>
2013-11-07 00:17:32 +0800

    *) Add promotion image.

--- src/test/java/com/idocv/docview/convert/Pdf2Html.java
+++ src/test/java/com/idocv/docview/convert/Pdf2Html.java
@@ -10,9 +10,10 @@ public class Pdf2Html {
 
 	public static void main(String[] args) {
 		pdf2html = "D:/idocv/bin/converter/pdf2html/pdf2htmlEX.exe";
+		pdf2html = "D:/test/pdf2html/pdf2htmlEX.exe";
 		String src = "D:/test/test.pdf";
 		String destDir = "d:/test/test";
-		String result = CmdUtil.runWindows(pdf2html, "--dest-dir", destDir.replaceAll("/", "\\\\"), src, "index.html");
+		String result = CmdUtil.runWindows(pdf2html, "--dest-dir", destDir.replaceAll("/", "\\\\"), "--embed", "cfijo", src, "index.html");
 		System.out.println(result);
 	}
 }
