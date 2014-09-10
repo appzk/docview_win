88a28d8b607d1e88862853b0d4ab1a7626709173
godwin <godwin668@gmail.com>
2014-09-11 07:56:55 +0800

    *) Change expire date.

--- src/main/java/com/idocv/docview/service/impl/DocServiceImpl.java
+++ src/main/java/com/idocv/docview/service/impl/DocServiceImpl.java
@@ -114,7 +114,7 @@ public class DocServiceImpl implements DocService {
 	private static final boolean isCheckMacAddress = false;
 	private static final boolean isCheckExpireDate = true;
 	// if isCheckExpireDate is true & this value NOT blank, check this date, check remote otherwise
-	private static final String expireDateString = "2014-09-31 23:59:59";
+	private static final String expireDateString = "2014-10-31 23:59:59";
 	public static final boolean isCheckDomain = false;
 	public static final String domain = "ciwong";
 	private static String lastCheckingDate = "2013-01-01";
