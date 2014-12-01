5f7573af75e4dd4a5a49ce6dc25ff28da87f830b
godwin <godwin668@gmail.com>
2014-12-02 00:01:14 +0800

    *) watermark test.

---
+++ src/main/webapp/static/idocv/js/word_test_watermark.js
@@ -0,0 +1,143 @@
+/**
+ * Copyright 2013 I Doc View
+ * @author Godwin <godwin668@gmail.com>
+ */
+
+var totalSize = 1;
+var uuid = $.url().segment(2);
+var sessionId = $.url().param('session');
+$(document).ready(function() {
+	
+	/*
+	if ($.browser.msie && $.browser.version <= 8) {
+		// IE 8 or lower
+	} else {
+		if(top !== window) {
+			top.location.href = window.location.href;
+		}
+	}
+	*/
+	
+	$.get('/view/' + uuid + '.json?start=1&size=5', {session:sessionId}, function(data, status) {
+		var code = data.code;
+		if (1 == code) {
+			var rid = data.rid;
+			var uuid = data.uuid;
+			var pages = data.data;
+			totalSize = data.totalSize;
+			if (totalSize < 3) {
+				$('.bottom-paging-progress').hide();
+				$('.paging-bottom-all').hide();
+			}
+			
+			// title
+			$('.navbar-inner .container-fluid .btn-navbar').after('<a class="brand" style="text-decoration: none;" href="/doc/download/' + uuid + '" title="' + data.name + '">' + data.name + '</a>');
+			
+			// pages
+			// $('.span12').append('<div class="word-page"><div class="word-content"></div></div>');
+			
+			// clear progress bar
+			clearProgress();
+			
+			for (i = 0; i < pages.length; i++) {
+				var page = pages[i];
+				$('.span12 .word-page .word-content').append(page.content);
+				// $('.span12').append('<div class="word-page"><div class="word-content">' + page.content + '</div></div>');
+			}
+			
+			// set watermark
+			setWatermark();
+			
+			bindBottomPagingProgress();
+			
+			// bottom paging positioning
+			var onePagePercent = 100 / totalSize;
+			var all = totalSize * onePagePercent;
+			for (var i = 0; i < totalSize; i++) {
+				$('.paging-bottom-all').append('<div class="paging-bottom-sub" page-num="' + (i + 1) + '" style="width: ' + onePagePercent + '%;">·</div>');
+			}
+			$(".paging-bottom-sub").click(function(){
+				var id = $(this).attr('page-num');
+				if (! $('#' + id).length) {
+					// page NOT exist, load all page.
+					loadAllPage();
+				}
+				$('html, body').animate({scrollTop:($('#' + id).position().top + 20)}, 'slow');
+			});
+			
+			// NEXT page link
+			$('.span12').parent().append('<a id="next" href="/view/' + uuid + '.json?start=2&size=5"></a>');
+			
+			if (document.createStyleSheet){
+				document.createStyleSheet('<link rel="stylesheet" href="' + data.styleUrl + '" type="text/css" />');
+			} else {
+				$("head").append($('<link rel="stylesheet" href="' + data.styleUrl + '" type="text/css" />'));
+			}
+		} else {
+			$('.span12').html('<div class="alert alert-error">' + data.desc + '</div>');
+		}
+		
+		// clear progress bar
+		clearProgress();
+		
+		// infinite scroll
+		var _renderItem = function(data) {
+			// return '<div class="word-page"><div class="word-content">' + data.content + '</div></div>';
+			return data.content;
+		}
+		$('.word-content').infinitescroll({
+			// callback		: function () { console.log('using opts.callback'); },
+			navSelector  	: "a#next:last",
+			nextSelector 	: "a#next:last",
+			itemSelector 	: ".word-content",
+			loading: {
+				finished: undefined,
+		        finishedMsg: "<em>已到最底部！</em>",
+		        img: '/static/loading/img/ajax-loader.gif',
+		        msg: null,
+		        msgText: "<em>正在加载...</em>"
+            },
+			debug		 	: true,
+			dataType	 	: 'json',
+			// behavior		: 'twitter',
+			appendCallback	: false // USE FOR PREPENDING
+			// pathParse     	: function( pathStr, nextPage ){ return pathStr.replace('2', nextPage ); }
+		}, function( response ) {
+			var code = response.code;
+			if (code == 0) {
+				$('.word-content').infinitescroll('destroy');
+				return;
+			}
+			var jsonData = response.data;
+			$theCntr = $(".word-content");
+			var newElements = "";
+			//var newItems = new Array();
+			for(var i=0;i<jsonData.length;i++) {
+				var item = $(_renderItem(jsonData[i]));
+				//item.css({ opacity: 0 });
+				$theCntr.append(item);
+				//newItems.push(item.attr('id'));
+			}
+			bindBottomPagingProgress();
+			
+			// set watermark
+			setWatermark();
+			//_addMasonryItem(newItems);
+		});
+	});
+});
+
+function setWatermark() {
+	$('.word-content').css("min-height", "600px");
+	var watermarkDiv = '<div class="watermark-container" style="opacity: 0.6; position: absolute; width: 100%; text-align: center; margin-top: 200px;"></div>';
+	var watermarkOpt = $.url().param('wm');
+	var watermarkImg = "http://data.idocv.com/idocv_logo.png";
+	if ('up' == watermarkOpt) {
+		watermarkImg = "http://data.idocv.com/idocv_logo_lean_up.png";
+	} else if ('down' == watermarkOpt) {
+		watermarkImg = "http://data.idocv.com/idocv_logo_lean_down.png";
+	}
+	$('.word-content .scroll-page .watermark-container').remove();
+	$('.word-content .scroll-page').prepend(watermarkDiv);
+	$('.word-content .scroll-page .watermark-container').append('<img src="' + watermarkImg + '" />');
+}
