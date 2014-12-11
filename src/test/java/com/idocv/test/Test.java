e96266f1b2c4ea138019ea246e9004cddc483939
godwin <godwin668@gmail.com>
2014-12-11 18:07:42 +0800

    3.5.6_20141211

--- src/test/java/com/idocv/test/Test.java
+++ src/test/java/com/idocv/test/Test.java
@@ -1,14 +1,19 @@
 package com.idocv.test;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.idocv.docview.service.impl.ClusterServiceImpl;
 
 public class Test {
 	public static void main(String[] args) {
-		String time = "2014-02-17 04:57:07";
+		String multiUrl = "node1@http://dfs1.idocv.com/doc/upload#node2@http://dfs2.idocv.com/doc/upload";
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("node", "node2");
+		System.out.println(params.get("node"));
+		String node = (String) params.get("node");
+		String nodeUrl = ClusterServiceImpl.getNodeUrl(multiUrl, node);
+		System.out.println("node URL: " + nodeUrl);
 
-		String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-		System.out.println("cur:" + currentDate);
-		System.out.println(time.startsWith(currentDate));
 	}
 }
