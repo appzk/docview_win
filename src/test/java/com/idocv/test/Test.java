1c35628e29c38a442cb03057bd28ae892dd0fc59
godwin668 <godwin668@gmail.com>
2015-06-09 23:53:47 +0800

    +) Add 7z command

--- src/test/java/com/idocv/test/Test.java
+++ src/test/java/com/idocv/test/Test.java
@@ -4,12 +4,10 @@ import com.idocv.docview.util.CmdUtil;
 
 public class Test {
 	public static void main(String[] args) {
-		String pdf2html = "D:/pdf2htmlEX/pdf2htmlEX.exe";
-		String src = "D:/pdf2htmlEX/test.pdf";
-		String destDir = "D:\\pdf2htmlEX\\ttt";
-		String result = CmdUtil.runWindows(pdf2html, "--embed", "cfijo",
-				"--fallback", "1", "--dest-dir", destDir, src, "index.html");
-		System.out.println("result:\n" + result);
-
+		String cmd = "D:/zip/7z.exe";
+		String zipSrc = "D:/zip/test.rar";
+		String destDir = "D:/zip/ttt";
+		String result = CmdUtil.runWindows(cmd, "e", zipSrc, "-o" + destDir, "-r", "-y");
+		System.out.println("result: " + result);
 	}
 }
