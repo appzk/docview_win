8dc9641460212783a3734fee8ee051a73cf5f4d1
godwin668 <godwin668@gmail.com>
2015-07-05 06:35:42 +0800

    *) Remove afterLoad in mobile ppt view.

--- src/main/webapp/static/idocv/js/ppt-mobile.js
+++ src/main/webapp/static/idocv/js/ppt-mobile.js
@@ -50,8 +50,6 @@ $(document).ready(function() {
 			
 			// photoswipe init
 			initPhotoSwipeFromDOM('.my-gallery');
-			
-			afterLoad();
 		} else {
 			$('.container-fluid .row-fluid').html('<section><div class="alert alert-error">' + data.desc + '</div></section>');
 		}
