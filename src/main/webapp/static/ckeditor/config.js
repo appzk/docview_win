0c0565d9e5bd853bb6dfb5336ad192eb4bf0b95d
godwin668 <godwin668@gmail.com>
2016-02-27 01:33:18 +0800

    *) Upgrade CKEditor to Version 4.5.7 • 4 Feb 2016

--- src/main/webapp/static/ckeditor/config.js
+++ src/main/webapp/static/ckeditor/config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -22,8 +22,7 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
+		{ name: 'colors' }
 	];
 
 	// Remove some buttons provided by the standard plugins, which are
