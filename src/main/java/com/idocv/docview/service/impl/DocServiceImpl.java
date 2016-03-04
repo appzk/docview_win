c420700e4ceecf805a54d89449f4af736304b38c
godwin668 <godwin668@gmail.com>
2016-03-04 10:42:33 +0800

    update

--- src/main/java/com/idocv/docview/service/impl/DocServiceImpl.java
+++ src/main/java/com/idocv/docview/service/impl/DocServiceImpl.java
@@ -117,7 +117,7 @@ public class DocServiceImpl implements DocService {
 	private static final boolean isCheckMacAddress = false;
 	private static final boolean isCheckExpireDate = false;
 	// if isCheckExpireDate is true & this value NOT blank, check this date, check remote otherwise
-	private static final String expireDateString = "2016-03-31 23:59:59";
+	private static final String expireDateString = "2016-04-30 23:59:59";
 	public static final boolean isCheckDomain = false;
 	public static final String domain = "ciwong";
 	private static String lastCheckingDate = "2013-01-01";
